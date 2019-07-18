43c7deabfc812bd70757cd1df3b82f6b2af01b7e
Josemaria <josemarianriagu25@gmail.com>
2019-07-18 22:10:20 +0800

    implement GET recipe ingredients api

--- recipes/recipeRouter.js
+++ recipes/recipeRouter.js
@@ -13,4 +13,14 @@ router.get('/', async (req, res) => {
    }
 })
 
+router.get('/:id/shoppinglist', async (req, res) => {
+   try {
+      const { id } = req.params;
+      const ingredients = await Recipes.getShoppingList(id);
+      res.status(200).json(ingredients)
+   } catch (error) {
+      res.status(500).json({ message: 'Oops! Don\'t set the kitchen on fire yet' })
+   }
+})
+
 module.exports = router;
