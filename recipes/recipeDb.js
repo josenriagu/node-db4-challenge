3b32507f59ef27cf7a3d452dc66ee3631107d2a2
Josemaria <josemarianriagu25@gmail.com>
2019-07-18 22:09:57 +0800

    define getShoppingList helper function

--- recipes/recipeDb.js
+++ recipes/recipeDb.js
@@ -2,8 +2,17 @@ const db = require('../data/dbConfig');
 
 module.exports = {
    getRecipes,
+   getShoppingList,
 };
 
 function getRecipes() {
    return db('recipes');
+}
+
+function getShoppingList(recipe_id) {
+   return db('recipeIngredients as ri')
+      .leftJoin('ingredients as i', 'i.id', 'ri.ingredient_id')
+      .select('ingredient_number', 'ingredient_name', 'qty')
+      .where({ recipe_id })
+      .orderBy('ingredient_number')
 }
